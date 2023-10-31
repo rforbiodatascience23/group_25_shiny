8a38040433a85934b8de870b4718949535fdca95
emilieengdal <s194360@student.dtu.dk>
2023-10-31 09:34:27 +0100

    Initial commit

--- R/app_server.R
+++ R/app_server.R
@@ -7,3 +7,16 @@
 app_server <- function(input, output, session) {
   # Your application server logic
 }
+
+# Copy in the following code in the module script when creating a new module
+'''
+if(FALSE){ # Testing
+  golem::detach_all_attached()
+  golem::document_and_reload()
+  ui <- mod_NAME_ui("NAME_ui_1") # replace NAME here
+  server <- function( input,output,session){
+    mod_NAME_server("NAME_ui_1") # and here
+  }
+  shiny::shinyApp(ui, server)
+}
+'''
