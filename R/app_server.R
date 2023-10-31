7a1b6babcb52056bfca93921029af6763b7f1c95
emilieengdal <s194360@student.dtu.dk>
2023-10-31 11:00:43 +0100

    Task 3.4

--- R/app_server.R
+++ R/app_server.R
@@ -3,22 +3,32 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom ggplot2 theme
+#' @import centraldogma
 #' @noRd
 app_server <- function(input, output, session) {
   mod_plot_mod_server("plot_mod_1")
+  output$abundance <- renderPlot({
+    if(input$peptide == ""){
+      NULL
+    } else{
+      input$peptide |>
+        centraldogma::plot_character_frequency() +
+        ggplot2::theme(legend.position = "none")
+    }
+  })
 }
 
 
 
 # Copy in the following code in the module script when creating a new module
-'''
-if(FALSE){ # Testing
-  golem::detach_all_attached()
-  golem::document_and_reload()
-  ui <- mod_NAME_ui("NAME_ui_1") # replace NAME here
-  server <- function( input,output,session){
-    mod_NAME_server("NAME_ui_1") # and here
-  }
-  shiny::shinyApp(ui, server)
-}
-'''
+#
+# if(FALSE){ # Testing
+#   golem::detach_all_attached()
+#   golem::document_and_reload()
+#   ui <- mod_NAME_ui("NAME_ui_1") # replace NAME here
+#   server <- function( input,output,session){
+#     mod_NAME_server("NAME_ui_1") # and here
+#   }
+#   shiny::shinyApp(ui, server)
+# }
