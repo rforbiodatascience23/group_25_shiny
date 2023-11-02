082acc8734cfb8493f855b0ed72c0c6e3eedaf9a
jennifer-dtu-2023 <s230705@student.dtu.dk>
2023-11-02 02:29:31 +0100

    made changes to app.R

--- app.R
+++ app.R
@@ -2,11 +2,16 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+# Check if the 'centraldogma' package is installed. If not, prompt the user to install it.
 if (!requireNamespace("centraldogma", quietly = TRUE)) {
-  devtools::install_github("rforbiodatascience23/group_25_package")
+  stop("The 'centraldogma' package is not installed. Please install it using devtools::install_github('rforbiodatascience23/group_25_package')")
 }
-library(centraldogma)
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-source("R/mod_from_dna_to_pep.R")
+
+# Set production options
+options("golem.app.prod" = TRUE)
+
+# Source additional scripts if necessary
+# source("R/mod_from_dna_to_pep.R")
+
+# Run the app using the fully qualified namespace
 centraldogma::run_app() # add parameters here (if any)
