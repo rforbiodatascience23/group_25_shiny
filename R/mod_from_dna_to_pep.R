38507937db5cab21f75bc9bb91b868227cb085e6
jennifer-dtu-2023 <s230705@student.dtu.dk>
2023-11-02 03:20:02 +0100

    Revert to old-commit-1f9f664b7e274f73cdf4d340575a91d08f399270

--- R/mod_from_dna_to_pep.R
+++ R/mod_from_dna_to_pep.R
@@ -36,37 +36,8 @@ mod_from_dna_to_pep <- function(id){
 #' @noRd
 mod_from_dna_to_pep_server <- function(id){
   moduleServer( id, function(input, output, session){
-    dna <- reactiveVal() #object should react to user input
     ns <- session$ns
-    output$DNA <- renderUI({ #create ui field
-      textAreaInput(
-        inputId = ns("DNA"),
-        label = "DNA sequence",
-        placeholder = "Insert DNA sequence",
-        value = dna(),
-        height = 100,
-        width = 600
-      )
-    })
-    observeEvent(input$generate_dna, {
-      dna(
-        centraldogma::generate_DNA(input$DNA_length) #insert package function 1
-      )
-    })
-    output$peptide <- renderText({
-      # Ensure input is not NULL (loading the app) and is longer than 2 characters
-      if(is.null(input$DNA)){
-        NULL
-      } else if(nchar(input$DNA) < 3){
-        NULL
-      } else{
-        input$DNA |>
-          toupper() |> #input is uppercase
-          centraldogma::transcription() |>
-          centraldogma::mRNA_to_codons() |>
-          centraldogma::translation()
-      }
-    })
+
   })
 }
 
